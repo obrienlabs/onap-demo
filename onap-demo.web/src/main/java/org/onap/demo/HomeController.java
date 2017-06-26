d7d01ad6cf625330317f6dfb82fa8587d4c6c69c
Michael O'Brien <michael@obrienlabs.org>
2017-06-26 08:57:08 -0400

    #1 start adding reactjs

--- onap-demo.web/src/main/java/org/onap/demo/HomeController.java
+++ onap-demo.web/src/main/java/org/onap/demo/HomeController.java
@@ -1,14 +1,17 @@
 package org.onap.demo;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+//@RestController
+@Controller
 public class HomeController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        //return "Greetings from Spring Boot!";
+    	return "index.html";
     }
 
 }
